8999f07c258adfc27b4d7ea1823dbb77cef99b2c
Neobis <neobis@iMac-Neobis.local>
2022-06-20 14:17:21 +0600

    start of project, added buttons and labels

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Neobis on 20/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var minutesLabel: UILabel!
+    @IBOutlet weak var secundesLabel: UILabel!
+    
+    
+    @IBAction func softButtonTouched(_ sender: UIButton) {
+    }
+    @IBAction func mediumButtonTouched(_ sender: UIButton) {
+    }
+    @IBAction func hardButtonTouched(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
