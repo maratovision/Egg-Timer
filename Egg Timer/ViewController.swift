17f15d3371874e00cec38a0feece67461d067daf
Neobis <neobis@iMac-Neobis.local>
2022-06-20 20:10:29 +0600

    added logic for buttons and constrains, made a timer

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,27 +9,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
+    // MARK: - Outlets
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var softButton: UIButton!
+    @IBOutlet weak var mediumButton: UIButton!
+    @IBOutlet weak var hardButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var minutesLabel: UILabel!
-    @IBOutlet weak var secundesLabel: UILabel!
+
+    var totalTime = 0
     
-    
-    @IBAction func softButtonTouched(_ sender: UIButton) {
+    var target = 0 {
+        didSet {
+            let min = String(format: "%02d", minutes)
+            let sec = String(format: "%02d", seconds)
+            timeLabel.text = "\(min):\(sec)"
+            progressBar.setProgress(.zero, animated: true)
+        }
     }
-    @IBAction func mediumButtonTouched(_ sender: UIButton) {
-    }
-    @IBAction func hardButtonTouched(_ sender: UIButton) {
+    
+    var seconds: Int {
+        target % 60
     }
     
+    var minutes: Int {
+        target / 60
+    }
     
+    var timer: Timer?
+
+    // MARK: - Actions
+    
+    @IBAction func eggButtonTouched(_ sender: UIButton) {
+        let text: String
+        switch sender {
+        case softButton:
+            text = "You choised soft mode"
+            target = 5 * 60
+        case mediumButton:
+            text = "You choised medium mode"
+            target = 10 * 60
+        case hardButton:
+            text = "You choised hard mode"
+            target = 15 * 60
+        default:
+            text = "Sabit chort"
+        }
+        totalTime = target
+
+        timer?.invalidate()
+        timer = nil
+        resultLabel.text = text
+    }
+    
+    @IBAction func startTouched(_ sender: UIButton) {
+        guard timer == nil else {return}
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            self.target -= 1
+            let diff = self.totalTime - self.target
+            let division: Float = Float(diff) / Float(self.totalTime)
+            self.progressBar.setProgress(division, animated: true)
+            self.resultLabel.text = "Cooking is in progress"
+            if self.target == .zero {
+                timer.invalidate()
+                self.resultLabel.text = "Your eggs are ready"
+                return
+            }
+        }
+    }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
 
 }
 
